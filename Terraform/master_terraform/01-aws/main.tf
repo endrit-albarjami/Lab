495c5cc07ef388f59f3ab1ca4fb794fd5ad337bd
endrit-albarjami <albarjami_endrit@live.com>
2025-08-10 16:58:03 +0200

    Final adjustments after history cleanup

--- Terraform/master_terraform/01-aws/main.tf
+++ Terraform/master_terraform/01-aws/main.tf
@@ -10,8 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-central-1" # Ensure this is correct for your desired region
-  # REMOVE access_key and secret_key if they are still here!
-  # They MUST NOT be committed to Git.
 }
 
 # Create a VPC
@@ -43,7 +41,7 @@ resource "aws_default_route_table" "main_vpc_default_rt" {
     "Name" = "my-default-rt"
   }
 
-} 
+}
 
 # Create an Internet Gateway
 resource "aws_internet_gateway" "my_web_igw" {
@@ -80,13 +78,37 @@ resource "aws_default_security_group" "default_sec_group" {
   }
 }
 
+data "aws_ami" "latest_amazon_linux2"{
+  owners = ["amazon"]
+  most_recent = true
+  filter{
+    name = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+
 resource "aws_instance" "my_vm" {
-  ami                    = "ami-0af9b40b1a16fe700"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.web.id
-  vpc_security_group_ids = [aws_default_security_group.default_sec_group.id]
+  ami                         = data.aws_ami.latest_amazon_linux2.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.web.id
+  vpc_security_group_ids      = [aws_default_security_group.default_sec_group.id]
   associate_public_ip_address = true
-  key_name               = "production_ssh_key"
+  key_name                    = "production_ssh_key"
+
+  # Corrected: Use user_data to execute the script on instance launch
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum -y update && sudo yum -y install httpd
+    sudo systemctl start httpd && sudo systemctl enable httpd
+    sudo echo "<h1>Deployed via Terraform</h1>" > /var/www/html/index.html
+  EOF
+
   tags = {
     "Name" = "My EC2 Instance - Amazon Linux 2"
   }
