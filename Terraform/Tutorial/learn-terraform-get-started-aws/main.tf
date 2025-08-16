8a7679b9e6d690dfe43de9332bbb46f3f4d587c2
endrit-albarjami <albarjami_endrit@live.com>
2025-08-16 10:41:39 +0200

    Manage & Destroy Resources

--- Terraform/Tutorial/learn-terraform-get-started-aws/main.tf
+++ Terraform/Tutorial/learn-terraform-get-started-aws/main.tf
@@ -13,12 +13,31 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+/*
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
+  
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
 
   tags = {
-    Name = "learn-terraform"
+  Name = var.instance_name
   }
+}
+*/
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames    = true
 }
 
